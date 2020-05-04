1def7653615dc3b80e46faf8080c52028e7b4399
Nicole Foster <63244300+nfoster1219@users.noreply.github.com>
2020-05-04 08:24:42 -0500

    commit one

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+let oneNumShots = 1
+const oneShots = document.querySelector("#teamone-shoot-button");
+oneShots.addEventListener("click", function Random() {
+    console.log ("Team one shot taken");
+    document.getElementById("teamone-numshots").innerText +++ oneNumShots;
+    document.getElementById("teamone-numgoals").innerText -= Math.floor(Math.random() * 11);
+})
+
+
+
+let twoNumShots = 1
+const twoShots = document.querySelector("#teamtwo-shoot-button");
+twoShots.addEventListener("click", function Random() {
+    console.log ("Team two shot taken");
+    document.getElementById("teamtwo-numshots").innerText +++ twoNumShots;
+    document.getElementById("teamtwo-numgoals").innerText -= Math.floor(Math.random() * 11);
+})
+
+let numResets = 1
+const resets = document.querySelector("#reset-button");
+resets.addEventListener("click", function () {
+    console.log ("Game Reset");
+    document.getElementById("num-resets").innerText +++ numResets;
+    document.getElementById("teamone-numshots").innerText = 0;
+    document.getElementById("teamone-numgoals").innerText = 0;
+    document.getElementById("teamtwo-numshots").innerText = 0;
+    document.getElementById("teamtwo-numgoals").innerText = 0;
+})
