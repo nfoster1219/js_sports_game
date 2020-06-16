121a711182db638efee35838cd9acb804960cdfb
nfoster1219 <63244300+nfoster1219@users.noreply.github.com>
2020-06-16 13:00:16 -0500

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,19 @@
 let oneNumShots = 1
 
 const oneShots = document.querySelector("#teamone-shoot-button");
+let oneNumShotsElements = document.querySelector('#teamone-numshots');
+let teamOneNumGoals = document.querySelector('#teamone-numgoals');
 oneShots.addEventListener("click", function Random() {
     console.log ("Team one shot taken");
-    document.getElementById("teamone-numshots").innerText +++ oneNumShots;
-    document.getElementById("teamone-numgoals").innerHTML -= Math.floor(Math.random() * 11);
+    oneNumShotsElements.innerHTML = Number(oneNumShotsElements.innerHTML) + 1
+    
+    if (Math.random() * 100 < 65) {
+        teamOneNumGoals.innerHTML = Number(teamOneNumGoals.innerHTML) + 1;
+        }
+   
 })
+
+
 
         
 
@@ -14,12 +22,19 @@ oneShots.addEventListener("click", function Random() {
 
 let twoNumShots = 1
 const twoShots = document.querySelector("#teamtwo-shoot-button");
+let twoNumShotsElements = document.querySelector ('#teamtwo-numshots');
+let teamTwoNumGoals = document.querySelector ('#teamtwo-numgoals')
 twoShots.addEventListener("click", function Random() {
     console.log ("Team two shot taken");
-    document.getElementById("teamtwo-numshots").innerText +++ twoNumShots;
-    document.getElementById("teamtwo-numgoals").innerText -= Math.floor(Math.random() * 11);
-})
+    twoNumShotsElements.innerHTML = Number(twoNumShotsElements.innerHTML) + 1
+    
+    if (Math.random() * 100 < 65) {
+        teamTwoNumGoals.innerHTML = Number(teamTwoNumGoals.innerHTML) + 1;
+        }
+    })
 
+
+    
 let numResets = 1
 const resets = document.querySelector("#reset-button");
 resets.addEventListener("click", function () {
